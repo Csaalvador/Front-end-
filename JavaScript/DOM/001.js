48d49bd98e0d5512ece411dceb568a07aff7a129
Csaalvador <caua.lima@aluno.ifsp.edu.br>
2023-05-10 22:59:42 -0300

    UP

--- JavaScript/DOM/001.js
+++ JavaScript/DOM/001.js
@@ -1,15 +1,14 @@
-const form = document.querySelector('form');
-const ul = document.querySelector('ul')
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
 
+form.onsubmit = function (event) {
+  event.preventDefault();
 
-form.onsubmit = function(event){
-    event.preventDefault()
+  const input = form.querySelector("input");
+  const value = input.value;
 
-    const input = form.querySelector('input')
-    const value = input.value
-
-    ////////////////////////// PRIMEIRA MANEIRA /////////////////////////////
-    /*
+  ////////////////////////// PRIMEIRA MANEIRA /////////////////////////////
+  /*
     const li = document.createElement('li')
     li.setAttribute("class", "px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out flex justify-between")
     li.textContent = value
@@ -22,24 +21,29 @@ form.onsubmit = function(event){
     ul.appendChild(li)
     */
 
-    /////////////////////////SEGUNDA MANEIRA///////////////////
+  /////////////////////////SEGUNDA MANEIRA///////////////////
 
-    /*
+  /*
     ul.innerHTML += `<li class="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out flex justify-between"><span>${value}</span><button class="text-red-700 delete">ⅹ</button></li>`
 
     input.value = ''
 */
 
-//////////////////////////////// MANEIRA MAIS SIMPLES////////////////////////:
+  //////////////////////////////// MANEIRA MAIS SIMPLES////////////////////////:
 
-if(value == "") return alert ("Não é possível listar um nome inexistente")
+  if (value == "") return alert("Não é possível listar um nome inexistente");
 
-const li = ul.querySelector('li').cloneNode(true)
-li.querySelector('span').textContent = value
-ul.appendChild(li)
+  const li = ul.querySelector("li").cloneNode(true);
+  li.querySelector("span").textContent = value;
+  ul.appendChild(li);
 
-input.value = ""
+  input.value = "";
+};
 
-
-
+ul.onclick = function (event){
+  if(event.target.classList.contains('delete')){
+    if(confirm("Tem certeza?")){
+    event.target.parentElement.remove()
+  }
+}
 }
