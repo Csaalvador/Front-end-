1a7b2d3d3a1a2f5f9aa7a15f27574c17b9709486
Csaalvador <caua.lima@aluno.ifsp.edu.br>
2023-05-18 17:05:38 -0300

    UP

--- JavaScript/Assistence/001array.js
+++
@@ -1,8 +0,0 @@
-//in javascripy array is a object and it is a collection of homogenous data type
-
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(arr);
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-
