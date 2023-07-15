abdb74c598303bc7a027467d7bc1c2e6b5d14727
Csaalvador <caua.lima@aluno.ifsp.edu.br>
2023-07-15 11:38:35 -0300

    feat: :art:

--- React/meu-projeto/src/App.js
+++ React/meu-projeto/src/App.js
@@ -8,10 +8,6 @@ function App() {
   return (
     <div className="App"> 
   
-    <h1>Testando Eventos</h1>
-    <Event numero ='1' />
-    <Event numero ='2' />
-<hr></hr>
 
     <Form />
 
--- React/meu-projeto/src/components/form.jsx
+++ React/meu-projeto/src/components/form.jsx
@@ -1,17 +1,46 @@
+import { useState } from "react"
+
 function Form() {
 
 
     function cadastrarUser (e) {
         e.preventDefault()
-        console.log('Cadastrado!')
+        console.log(name)
+        console.log(idade)
+        console.log(password)
+        alert('Opaaaaaaaaa')
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+    const [idade, setIdade] = useState()
 
     return(
         <div>
         <h1>Meu Cadastro</h1>
 
         <form onSubmit={cadastrarUser}>
-            <input type="text" placeholder="Digitite seu nome:" />
+            <label htmlFor="name">Nome: </label>
+            <input 
+            type="text"
+             placeholder="Digitite seu nome:"
+             onChange = {(e) => setName(e.target.value) }
+             /> <br />
+           <br />
+            <label htmlFor="number">Idade: </label> 
+            <input 
+            type="number" 
+            placeholder="digite sua idade"
+            onChange={(e) => setIdade (e.target.value)}
+             /> <br />
+        <br />
+                   
+            <label htmlFor="password">Senha: </label> 
+            <input type="password"
+             placeholder="digite sua idade"
+             onChange={(e) => setPassword (e.target.value)}
+              />
+        
         <div>
             <input type="submit" value='Cadastrar' />
         </div>
